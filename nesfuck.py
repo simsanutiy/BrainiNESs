dc6873e96baaa918bdf977a352d63a3d484ba1d3
Михалыч <simsanutiy@gmail.com>
2018-11-20 19:06:24 +0400

    Update nesfuck.py
    Header and footer on C added

--- nesfuck.py
+++ nesfuck.py
@@ -28,6 +28,7 @@ def bf_parse(inp):
 
 def c_process(code):
     """Переводим это говно в C"""
+    output.write(header)
     tab = 1
     order = 0
     print(code)
@@ -84,10 +85,49 @@ def c_process(code):
             order += 1
 
         if code[order] == '}':
-            output.write('}')
+            output.write(footer)
             order += 1
             
         #order += 1
+
+header = """#define PPU_CTRL		*((unsigned char*)0x2000)
+#define PPU_MASK		*((unsigned char*)0x2001)
+#define PPU_STATUS		*((unsigned char*)0x2002)
+#define SCROLL			*((unsigned char*)0x2005)
+#define PPU_ADDRESS		*((unsigned char*)0x2006)
+#define PPU_DATA		*((unsigned char*)0x2007)
+
+unsigned char a[512];
+unsigned char a_pointer = 0;
+unsigned char index;
+const unsigned char PALETTE[]={
+0x1f, 0x00, 0x10, 0x20
+}; //	black, gray, lt gray, white
+
+void main (void) {
+		//	turn off the screen
+	PPU_CTRL = 0;
+	PPU_MASK = 0;
+	
+	//	load the palette
+	PPU_ADDRESS = 0x3f; 	//	set an address in the PPU of 0x3f00
+	PPU_ADDRESS = 0x00;
+"""
+
+footer = """
+//	reset the scroll position	
+	PPU_ADDRESS = 0;
+	PPU_ADDRESS = 0;
+	SCROLL = 0;
+	SCROLL = 0;
+	
+	//	turn on screen
+	PPU_CTRL = 0x90; 	//	screen is on, NMI on
+	PPU_MASK = 0x1e;
+	
+	//	infinite loop
+	while (1);
+}"""
 
 c_process(bf_parse(input_parse()))
 #output.write('#' + code)
